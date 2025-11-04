5885cc86023b8cf91f767f5dff938de5c8695460
sakshi220122 <sakshii.teli2004@gmail.com>
2025-11-04 15:44:54 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from sakshi hello  app on AWS!"
+    return "Hello from Readle app on AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
